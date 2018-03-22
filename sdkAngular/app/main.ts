0c68b13757d0d2693a5b8e0792a36fa899bb13ed
sis0k0 <stanimira.vlaeva@gmail.com>
2018-03-22 21:27:39 +0200

    wip

--- sdkAngular/app/main.ts
+++ sdkAngular/app/main.ts
@@ -1,3 +1,14 @@
+    require("ui/frame");
+    require("ui/frame/activity");
+require("./main-activity.android.ts");
+const application = require("application");
+require("ui/styling/style-scope");
+const appCssContext = require.context("~/", false, /^\.\/app\.(css|scss|less|sass)$/);
+global.registerWebpackModules(appCssContext);
+application.loadAppCss();
+
+
+
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
 
